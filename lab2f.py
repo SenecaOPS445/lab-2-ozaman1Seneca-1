de72be24e78fb66f7ce645412c22bcf0d263ba53
Owais Zaman <ozaman1@myseneca.ca>
2024-05-24 07:33:21 -0400

    Lab Scripts

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author: Owais Zaman
+#Author ID: ozaman1
+#Date Created: 2024/05/24
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Author: Owais Zaman
+#Author ID: ozaman1
+#Date Created: 2024/05/24
+
+import sys
+
+if len(sys.argv) != 2:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
